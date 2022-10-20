4e62710d5f545c21f64e05b24a881b7e4d93f3be
arishaku <arisha.kumar@intel.com>
2022-10-20 11:30:22 -0700

    Added screenshot and removed unwanted files

--- gimpov/plugins/superresolution-ov/superresolution-ov.py
+++ gimpov/plugins/superresolution-ov/superresolution-ov.py
@@ -1,16 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
 """
- .d8888b.  8888888 888b     d888 8888888b.       888b     d888 888
-d88P  Y88b   888   8888b   d8888 888   Y88b      8888b   d8888 888
-888    888   888   88888b.d88888 888    888      88888b.d88888 888
-888          888   888Y88888P888 888   d88P      888Y88888P888 888
-888  88888   888   888 Y888P 888 8888888P"       888 Y888P 888 888
-888    888   888   888  Y8P  888 888             888  Y8P  888 888
-Y88b  d88P   888   888   "   888 888             888   "   888 888
- "Y8888P88 8888888 888       888 888             888       888 88888888
-
-
 Perform superresolution on the current layer.
 """
 import gi
@@ -282,6 +272,7 @@ def run(procedure, run_mode, image, n_drawables, layer, args, data):
 
 class Superresolution(Gimp.PlugIn):
     ## Parameters ##
+    
     __gproperties__ = {
         "scale": (float, _("_Scale"), "Scale", 1, 4, 2, GObject.ParamFlags.READWRITE),
         "model_name": (
@@ -329,5 +320,5 @@ class Superresolution(Gimp.PlugIn):
             procedure.add_argument_from_property(self, "model_name")
         return procedure
 
-
+print("--------------before Superres GIMP main -----------------")
 Gimp.main(Superresolution.__gtype__, sys.argv)
