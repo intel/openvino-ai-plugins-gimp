4ffd3879dd9c32298f56ac9066ff86a24b3b5006
Long, Garth <garth.long@intel.com>
2023-11-18 14:59:12 -0700

    Removed old class, other LCM updates

--- gimpopenvino/tools/openvino_common/models_ov/stable_diffusion_engine.py
+++ gimpopenvino/tools/openvino_common/models_ov/stable_diffusion_engine.py
@@ -197,19 +197,13 @@ class StableDiffusionEngineAdvanced(DiffusionPipeline):
             #self.vae_encoder = None
             self.vae_encoder = self.core.compile_model(os.path.join(model, "vae_encoder.xml"), device[3])  #uncomment for prompt+init_image usecase.
 
-
-
-
         self._vae_d_output = self.vae_decoder.output(0)
         self._vae_e_output = self.vae_encoder.output(0) if self.vae_encoder is not None else None
-
-
 
         if self.unet.input("latent_model_input").shape[1] == 4:
             self.height = self.unet.input("latent_model_input").shape[2] * 8
             self.width = self.unet.input("latent_model_input").shape[3] * 8
         else:
-
             self.height = self.unet.input("latent_model_input").shape[1] * 8
             self.width = self.unet.input("latent_model_input").shape[2] * 8
 
@@ -340,25 +334,17 @@ class StableDiffusionEngineAdvanced(DiffusionPipeline):
             self.infer_request_time_proj.wait()
             time_proj = self.infer_request_time_proj.get_output_tensor(0).data.astype(np.float32)
 
-
-
              #Alternating between -prompt and +prompt on iGPI and NPU
             if self.swap:
-
                 if i % 2 == 0:
-
                     input_tens_neg_dict = {"time_proj": np.float32(time_proj), "latent_model_input":latent_model_input_neg, "encoder_hidden_states": np.expand_dims(text_embeddings[0], axis=0)}
                     input_tens_dict = {"time_proj": np.float32(time_proj), "latent_model_input":latent_model_input, "encoder_hidden_states": np.expand_dims(text_embeddings[1], axis=0)}
-
                 else:
-
                     input_tens_neg_dict = {"time_proj": np.float32(time_proj), "latent_model_input":latent_model_input_neg, "encoder_hidden_states": np.expand_dims(text_embeddings[1], axis=0)}
                     input_tens_dict = {"time_proj": np.float32(time_proj), "latent_model_input":latent_model_input, "encoder_hidden_states": np.expand_dims(text_embeddings[0], axis=0)}
-
             else:
                     input_tens_neg_dict = {"time_proj": np.float32(time_proj), "latent_model_input":latent_model_input_neg, "encoder_hidden_states": np.expand_dims(text_embeddings[0], axis=0)}
                     input_tens_dict = {"time_proj": np.float32(time_proj), "latent_model_input":latent_model_input, "encoder_hidden_states": np.expand_dims(text_embeddings[1], axis=0)}
-
 
             self.infer_request_neg.start_async(input_tens_neg_dict)
             self.infer_request.start_async(input_tens_dict)
@@ -366,9 +352,7 @@ class StableDiffusionEngineAdvanced(DiffusionPipeline):
             self.infer_request.wait()
 
             if self.swap:
-
                 if i % 2 == 0:
-
                     noise_pred_neg = self.infer_request_neg.get_output_tensor(0)
                     noise_pred_pos = self.infer_request.get_output_tensor(0)
                 else:
@@ -379,12 +363,8 @@ class StableDiffusionEngineAdvanced(DiffusionPipeline):
                     noise_pred_pos = self.infer_request.get_output_tensor(0)
 
 
-
-
             noise_pred.append(noise_pred_neg.data.astype(np.float32))
             noise_pred.append(noise_pred_pos.data.astype(np.float32))
-
-
 
             # perform guidance
             if do_classifier_free_guidance:
@@ -394,18 +374,11 @@ class StableDiffusionEngineAdvanced(DiffusionPipeline):
             # compute the previous noisy sample x_t -> x_t-1
             latents = scheduler.step(torch.from_numpy(noise_pred), t, torch.from_numpy(latents), **extra_step_kwargs)["prev_sample"].numpy()
 
-
-
-
-
-
             if create_gif:
                 frames.append(latents)
 
         if callback:
             callback(num_inference_steps, callback_userdata)
-
-
 
         # scale and decode the image latents with vae
         latents = 1 / 0.18215 * latents
@@ -415,8 +388,6 @@ class StableDiffusionEngineAdvanced(DiffusionPipeline):
         print("Decoder ended:",time.time() - start)
 
         image = self.postprocess_image(image, meta)
-        #print("postprocess_image ended")
-
 
         if create_gif:
             gif_folder=os.path.join(model,"../../../gif")
@@ -434,8 +405,6 @@ class StableDiffusionEngineAdvanced(DiffusionPipeline):
             frame_one = frames_image[0]
             gif_file=os.path.join(gif_folder,"stable_diffusion.gif")
             frame_one.save(gif_file, format="GIF", append_images=frames_image, save_all=True, duration=100, loop=0)
-
-        print("type(engine image) = ", type(image))
 
         return image
 
@@ -813,269 +782,6 @@ class StableDiffusionEngine(DiffusionPipeline):
 
         return timesteps, num_inference_steps - t_start
 
-class oldLatentConsistencyEngine(DiffusionPipeline):
-    def __init__(
-            self,
-            # scheduler: Union[DDIMScheduler, PNDMScheduler, LMSDiscreteScheduler],
-            model="SimianLuo/LCM_Dreamshaper_v7",
-            tokenizer="openai/clip-vit-large-patch14",
-            device=["CPU", "CPU", "CPU"],
-            blobs=False,
-            swap=False
-    ):
-        try:
-            self.tokenizer = CLIPTokenizer.from_pretrained(model, local_files_only=True)
-        except:
-            self.tokenizer = CLIPTokenizer.from_pretrained(tokenizer)
-            self.tokenizer.save_pretrained(model)
-
-        self.core = Core()
-        self.core.set_property({'CACHE_DIR': os.path.join(model, 'cache')})  # adding caching to reduce init time
-        # text features
-
-        print("Text Device:", device[0])
-        self.text_encoder = self.core.compile_model(os.path.join(model, "text_encoder.xml"), device[0])
-        self._text_encoder_output = self.text_encoder.output(0)
-
-        # diffusion
-        print("unet Device:", device[1])
-        self.unet = self.core.compile_model(os.path.join(model, "unet.xml"), device[1])
-        self._unet_output = self.unet.output(0)
-
-        # decoder
-        print("Vae Device:", device[2])
-
-        self.vae_decoder = self.core.compile_model(os.path.join(model, "vae_decoder.xml"), device[2])
-
-        pipe = DiffusionPipeline.from_pretrained(
-            "SimianLuo/LCM_Dreamshaper_v7",
-            custom_pipeline="latent_consistency_txt2img",
-            custom_revision="main",
-        )
-        self.scheduler = pipe.scheduler
-        self.safety_checker = pipe.safety_checker
-        self.feature_extractor = pipe.feature_extractor
-        self.vae_scale_factor = 2 ** 3
-        self.image_processor = VaeImageProcessor(vae_scale_factor=self.vae_scale_factor)
-        # models
-
-
-
-
-    def _encode_prompt(
-            self,
-            prompt,
-            num_images_per_prompt,
-            prompt_embeds: None,
-    ):
-        r"""
-        Encodes the prompt into text encoder hidden states.
-        Args:
-            prompt (`str` or `List[str]`, *optional*):
-                prompt to be encoded
-            num_images_per_prompt (`int`):
-                number of images that should be generated per prompt
-            prompt_embeds (`torch.FloatTensor`, *optional*):
-                Pre-generated text embeddings. Can be used to easily tweak text inputs, *e.g.* prompt weighting. If not
-                provided, text embeddings will be generated from `prompt` input argument.
-        """
-
-        if prompt_embeds is None:
-
-            text_inputs = self.tokenizer(
-                prompt,
-                padding="max_length",
-                max_length=self.tokenizer.model_max_length,
-                truncation=True,
-                return_tensors="pt",
-            )
-            text_input_ids = text_inputs.input_ids
-            untruncated_ids = self.tokenizer(
-                prompt, padding="longest", return_tensors="pt"
-            ).input_ids
-
-            if untruncated_ids.shape[-1] >= text_input_ids.shape[
-                -1
-            ] and not torch.equal(text_input_ids, untruncated_ids):
-                removed_text = self.tokenizer.batch_decode(
-                    untruncated_ids[:, self.tokenizer.model_max_length - 1: -1]
-                )
-                logger.warning(
-                    "The following part of your input was truncated because CLIP can only handle sequences up to"
-                    f" {self.tokenizer.model_max_length} tokens: {removed_text}"
-                )
-
-            prompt_embeds = self.text_encoder(text_input_ids, share_inputs=True, share_outputs=True)
-            prompt_embeds = torch.from_numpy(prompt_embeds[0])
-
-        bs_embed, seq_len, _ = prompt_embeds.shape
-        # duplicate text embeddings for each generation per prompt
-        prompt_embeds = prompt_embeds.repeat(1, num_images_per_prompt, 1)
-        prompt_embeds = prompt_embeds.view(
-            bs_embed * num_images_per_prompt, seq_len, -1
-        )
-
-        # Don't need to get uncond prompt embedding because of LCM Guided Distillation
-        return prompt_embeds
-
-    def run_safety_checker(self, image, dtype):
-        if self.safety_checker is None:
-            has_nsfw_concept = None
-        else:
-            if torch.is_tensor(image):
-                feature_extractor_input = self.image_processor.postprocess(
-                    image, output_type="pil"
-                )
-            else:
-                feature_extractor_input = self.image_processor.numpy_to_pil(image)
-            safety_checker_input = self.feature_extractor(
-                feature_extractor_input, return_tensors="pt"
-            )
-            image, has_nsfw_concept = self.safety_checker(
-                images=image, clip_input=safety_checker_input.pixel_values.to(dtype)
-            )
-        return image, has_nsfw_concept
-
-    def prepare_latents(
-            self, batch_size, num_channels_latents, height, width, dtype, latents=None
-    ):
-        shape = (
-            batch_size,
-            num_channels_latents,
-            height // self.vae_scale_factor,
-            width // self.vae_scale_factor,
-        )
-        if latents is None:
-            latents = torch.randn(shape, dtype=dtype)
-        # scale the initial noise by the standard deviation required by the scheduler
-        latents = latents * self.scheduler.init_noise_sigma
-        return latents
-
-    def get_w_embedding(self, w, embedding_dim=512, dtype=torch.float32):
-        """
-        see https://github.com/google-research/vdm/blob/dc27b98a554f65cdc654b800da5aa1846545d41b/model_vdm.py#L298
-        Args:
-        timesteps: torch.Tensor: generate embedding vectors at these timesteps
-        embedding_dim: int: dimension of the embeddings to generate
-        dtype: data type of the generated embeddings
-        Returns:
-        embedding vectors with shape `(len(timesteps), embedding_dim)`
-        """
-        assert len(w.shape) == 1
-        w = w * 1000.0
-
-        half_dim = embedding_dim // 2
-        emb = torch.log(torch.tensor(10000.0)) / (half_dim - 1)
-        emb = torch.exp(torch.arange(half_dim, dtype=dtype) * -emb)
-        emb = w.to(dtype)[:, None] * emb[None, :]
-        emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
-        if embedding_dim % 2 == 1:  # zero pad
-            emb = torch.nn.functional.pad(emb, (0, 1))
-        assert emb.shape == (w.shape[0], embedding_dim)
-        return emb
-
-
-    def __call__(
-            self,
-            prompt: Union[str, List[str]] = None,
-            height: Optional[int] = 512,
-            width: Optional[int] = 512,
-            guidance_scale: float = 7.5,
-            num_images_per_prompt: Optional[int] = 1,
-            latents: Optional[torch.FloatTensor] = None,
-            model=None,
-            scheduler=None,
-            num_inference_steps: int = 4,
-            lcm_origin_steps: int = 50,
-            prompt_embeds: Optional[torch.FloatTensor] = None,
-            output_type: Optional[str] = "pil",
-            return_dict: bool = True,
-            cross_attention_kwargs: Optional[Dict[str, Any]] = None,
-            callback = None,
-            callback_userdata = None
-    ):
-
-        # 1. Define call parameters
-        if prompt is not None and isinstance(prompt, str):
-            batch_size = 1
-        elif prompt is not None and isinstance(prompt, list):
-            batch_size = len(prompt)
-        else:
-            batch_size = prompt_embeds.shape[0]
-
-        # do_classifier_free_guidance = guidance_scale > 0.0
-        # In LCM Implementation:  cfg_noise = noise_cond + cfg_scale * (noise_cond - noise_uncond) , (cfg_scale > 0.0 using CFG)
-
-        # 2. Encode input prompt
-        prompt_embeds = self._encode_prompt(
-            prompt,
-            num_images_per_prompt,
-            prompt_embeds=prompt_embeds,
-        )
-
-        # 3. Prepare timesteps
-        self.scheduler.set_timesteps(num_inference_steps, lcm_origin_steps)
-        timesteps = self.scheduler.timesteps
-
-        # 4. Prepare latent variable
-        num_channels_latents = 4
-        latents = self.prepare_latents(
-            batch_size * num_images_per_prompt,
-            num_channels_latents,
-            height,
-            width,
-            prompt_embeds.dtype,
-            latents,
-        )
-
-        bs = batch_size * num_images_per_prompt
-
-        # 5. Get Guidance Scale Embedding
-        w = torch.tensor(guidance_scale).repeat(bs)
-        w_embedding = self.get_w_embedding(w, embedding_dim=256)
-
-        # 6. LCM MultiStep Sampling Loop:
-        with self.progress_bar(total=num_inference_steps) as progress_bar:
-            for i, t in enumerate(timesteps):
-                ts = torch.full((bs,), t, dtype=torch.long)
-
-                # model prediction (v-prediction, eps, x)
-                model_pred = \
-                self.unet([latents, ts, prompt_embeds, w_embedding], share_inputs=True, share_outputs=True)[0]
-
-                # compute the previous noisy sample x_t -> x_t-1
-                latents, denoised = self.scheduler.step(
-                    torch.from_numpy(model_pred), i, t, latents, return_dict=False
-                )
-                progress_bar.update()
-
-        if not output_type == "latent":
-            image = torch.from_numpy(self.vae_decoder(denoised / 0.18215, share_inputs=True, share_outputs=True)[0])
-            image, has_nsfw_concept = self.run_safety_checker(
-                image, prompt_embeds.dtype
-            )
-        else:
-            image = denoised
-            has_nsfw_concept = None
-
-        if has_nsfw_concept is None:
-            do_denormalize = [True] * image.shape[0]
-        else:
-            do_denormalize = [not has_nsfw for has_nsfw in has_nsfw_concept]
-
-        image = self.image_processor.postprocess(
-            image, output_type=output_type, do_denormalize=do_denormalize
-        )
-        print("type(engine image) = ", type(image[0]))
-
-        return image[0]
-        #if not return_dict:
-        #    return (image, has_nsfw_concept)
-
-        #return StableDiffusionPipelineOutput(
-        #    images=image, nsfw_content_detected=has_nsfw_concept
-        #)
-
 class LatentConsistencyEngine(DiffusionPipeline):
     def __init__(
         self,
@@ -1261,7 +967,8 @@ class LatentConsistencyEngine(DiffusionPipeline):
         else:
             batch_size = prompt_embeds.shape[0]
 
-        torch.manual_seed(seed)
+        if seed is not None:
+            torch.manual_seed(seed)
 
         #print("After Step 1: batch size is ", batch_size)
         # do_classifier_free_guidance = guidance_scale > 0.0
@@ -1335,5 +1042,4 @@ class LatentConsistencyEngine(DiffusionPipeline):
             image, output_type=output_type, do_denormalize=do_denormalize
         )
 
-        print("Ready to return: image is ", image)
         return image[0]
