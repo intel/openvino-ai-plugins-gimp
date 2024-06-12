33ab0dffd0a71b6fa6da27b45fe6b6ffd69333fb
Long, Garth <garth.long@intel.com>
2024-06-11 17:27:40 -0700

    light refactoring, and removing NPU from SuperRes device list (for now).

--- gimpopenvino/plugins/superresolution-ov/superresolution-ov.py
+++ gimpopenvino/plugins/superresolution-ov/superresolution-ov.py
@@ -7,22 +7,23 @@
 Perform superresolution on the current layer.
 """
 import gi
-gi.require_version("Gimp", "3.0")
-gi.require_version("GimpUi", "3.0")
-gi.require_version("Gtk", "3.0")
-from gi.repository import Gimp, GimpUi, GObject, GLib, Gio, Gtk
 import gettext
 import subprocess
 import json
 import os
 import sys
+from gi.repository import Gimp, GimpUi, GObject, GLib, Gio, Gtk
+
 sys.path.extend([os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")])
 from plugin_utils import *
 
+gi.require_version("Gimp", "3.0")
+gi.require_version("GimpUi", "3.0")
+gi.require_version("Gtk", "3.0")
 
 _ = gettext.gettext
-image_paths = {
 
+image_paths = {
     "logo": os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "..", "images", "plugin_logo.png"
     ),
@@ -32,64 +33,56 @@ image_paths = {
 }
 
 class StringEnum:
-    """
-    Helper class for when you want to use strings as keys of an enum. The values would be
-    user facing strings that might undergo translation.
-
-    The constructor accepts an even amount of arguments. Each pair of arguments
-    is a key/value pair.
-    """
-
     def __init__(self, *args):
         self.keys = []
         self.values = []
-
         for i in range(len(args) // 2):
             self.keys.append(args[i * 2])
             self.values.append(args[i * 2 + 1])
 
     def get_tree_model(self):
-        """Get a tree model that can be used in GTK widgets."""
         tree_model = Gtk.ListStore(GObject.TYPE_STRING, GObject.TYPE_STRING)
         for i in range(len(self.keys)):
             tree_model.append([self.keys[i], self.values[i]])
         return tree_model
-        
-        
+
 class DeviceEnum:
-
-
     def __init__(self, supported_devices):
-        self.keys = []
-        self.values = [] 
-        for i in supported_devices:
-            
-            self.keys.append(i)
-            self.values.append(i)
+        self.keys = supported_devices
+        self.values = supported_devices
 
     def get_tree_model(self):
-        """Get a tree model that can be used in GTK widgets."""
         tree_model = Gtk.ListStore(GObject.TYPE_STRING, GObject.TYPE_STRING)
         for i in range(len(self.keys)):
             tree_model.append([self.keys[i], self.values[i]])
         return tree_model
 
-
 model_name_enum = StringEnum(
-    "esrgan",
-    _("esrgan"),
-    "sr_1033",
-    _("sr_1033"),
-       "sr_1032",
-    _("sr_1032"),
-
+    "esrgan", _("esrgan"),
+    "sr_1033", _("sr_1033"),
+    "sr_1032", _("sr_1032"),
 )
 
+def save_inference_parameters(weight_path, device_name, scale, model_name):
+    parameters = {
+        "device_name": device_name,
+        "scale": float(scale),
+        "model_name": model_name,
+        "inference_status": "started"
+    }
+    with open(os.path.join(weight_path, "..", "gimp_openvino_run.json"), "w") as file:
+        json.dump(parameters, file)
 
+def load_inference_results(weight_path):
+    with open(os.path.join(weight_path, "..", "gimp_openvino_run.json"), "r") as file:
+        return json.load(file)
 
+def remove_temporary_files(directory):
+    for f_name in os.listdir(directory):
+        if f_name.startswith("cache"):
+            os.remove(os.path.join(directory, f_name))
 
-def superresolution(procedure, image, drawable,scale, device_name, model_name, progress_bar, config_path_output):
-    # Save inference parameters and layers
+def superresolution(procedure, image, drawable, scale, device_name, model_name, progress_bar, config_path_output):
     weight_path = config_path_output["weight_path"]
     python_path = config_path_output["python_path"]
     plugin_path = config_path_output["plugin_path"]
@@ -98,22 +91,14 @@ def superresolution(procedure, image, drawable,scale, device_name, model_name, p
     image.undo_group_start()
 
     save_image(image, drawable, os.path.join(weight_path, "..", "cache.png"))
+    save_inference_parameters(weight_path, device_name, scale, model_name)
 
-    #with open(os.path.join(weight_path, "..", "gimp_openvino_run.pkl"), "wb") as file:
-     #   pickle.dump({"device_name": device_name, "scale": float(scale),"model_name": model_name, "inference_status": "started"}, file)
-    with open(os.path.join(weight_path, "..", "gimp_openvino_run.json"), "w") as file:
-        json.dump({"device_name": device_name, "scale": float(scale), "model_name": model_name, "inference_status": "started"}, file)
-
-
-    # Run inference and load as layer
-    #data_output = subprocess.call([python_path, plugin_path, device_name, str(scale), model_name])
     subprocess.call([python_path, plugin_path])
 
-    with open(os.path.join(weight_path, "..", "gimp_openvino_run.json"), "r") as file:
-        data_output = json.load(file)
+    data_output = load_inference_results(weight_path)
     image.undo_group_end()
     Gimp.context_pop()
-    #scale = 3
+
     if data_output["inference_status"] == "success":
         if scale == 1:
             result = Gimp.file_load(
@@ -123,41 +108,30 @@ def superresolution(procedure, image, drawable,scale, device_name, model_name, p
             result_layer = result.get_active_layer()
             copy = Gimp.Layer.new_from_drawable(result_layer, image)
             copy.set_name("SuperResolution")
-            copy.set_mode(Gimp.LayerMode.NORMAL_LEGACY)  # DIFFERENCE_LEGACY
+            copy.set_mode(Gimp.LayerMode.NORMAL_LEGACY)
             image.insert_layer(copy, None, -1)
         else:
             image_new = Gimp.Image.new(
                 drawable[0].get_width() * scale, drawable[0].get_height() * scale, 0
-            )  # 0 for RGB
+            )
             display = Gimp.Display.new(image_new)
             result = Gimp.file_load(
                 Gimp.RunMode.NONINTERACTIVE,
                 Gio.File.new_for_path(os.path.join(weight_path, "..", "cache.png")),
             )
             try:
-                # 2.99.10
                 result_layer = result.get_active_layer()
             except:
-                # > 2.99.10
                 result_layers = result.list_layers()
                 result_layer = result_layers[0]
             copy = Gimp.Layer.new_from_drawable(result_layer, image_new)
             copy.set_name("SuperResolution")
-            copy.set_mode(Gimp.LayerMode.NORMAL_LEGACY)  # DIFFERENCE_LEGACY
+            copy.set_mode(Gimp.LayerMode.NORMAL_LEGACY)
             image_new.insert_layer(copy, None, -1)
 
         Gimp.displays_flush()
-        image.undo_group_end()
-        Gimp.context_pop()
-
-        # Remove temporary layers that were saved
-        my_dir = os.path.join(weight_path, "..")
-        for f_name in os.listdir(my_dir):
-            if f_name.startswith("cache"):
-                os.remove(os.path.join(my_dir, f_name))
-
+        remove_temporary_files(os.path.join(weight_path, ".."))
         return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
-
     else:
         show_dialog(
             "Inference not successful. See error_log.txt in GIMP-OpenVINO folder.",
@@ -167,20 +141,19 @@ def superresolution(procedure, image, drawable,scale, device_name, model_name, p
         )
         return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
 
-
 def run(procedure, run_mode, image, n_drawables, layer, args, data):
     scale = args.index(0)
     device_name = args.index(1)
     model_name = args.index(2)
 
     if run_mode == Gimp.RunMode.INTERACTIVE:
-        # Get all paths
         config_path = os.path.join(
             os.path.dirname(os.path.realpath(__file__)), "..", "..", "tools"
         )
- 
+
         with open(os.path.join(config_path, "gimp_openvino_config.json"), "r") as file:
             config_path_output = json.load(file)
+        
         python_path = config_path_output["python_path"]
         config_path_output["plugin_path"] = os.path.join(config_path, "superresolution-ov.py")
         
@@ -190,21 +163,16 @@ def run(procedure, run_mode, image, n_drawables, layer, args, data):
         config.begin_run(image, run_mode, args)
 
         GimpUi.init("superresolution-ov.py")
-        use_header_bar = Gtk.Settings.get_default().get_property(
-            "gtk-dialogs-use-header"
-        )
+        use_header_bar = Gtk.Settings.get_default().get_property("gtk-dialogs-use-header")
         dialog = GimpUi.Dialog(use_header_bar=use_header_bar, title=_("SuperResolution..."))
         dialog.add_button("_Cancel", Gtk.ResponseType.CANCEL)
         dialog.add_button("_Help", Gtk.ResponseType.APPLY)
         dialog.add_button("_Generate", Gtk.ResponseType.OK)
 
-        vbox = Gtk.Box(
-            orientation=Gtk.Orientation.VERTICAL, homogeneous=False, spacing=10
-        )
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, homogeneous=False, spacing=10)
         dialog.get_content_area().add(vbox)
         vbox.show()
 
-        # Create grid to set all the properties inside.
         grid = Gtk.Grid()
         grid.set_column_homogeneous(False)
         grid.set_border_width(10)
@@ -213,7 +181,6 @@ def run(procedure, run_mode, image, n_drawables, layer, args, data):
         vbox.add(grid)
         grid.show()
 
-       # Scale parameter
         label = Gtk.Label.new_with_mnemonic(_("_Scale"))
         grid.attach(label, 0, 2, 1, 1)
         label.show()
@@ -223,7 +190,6 @@ def run(procedure, run_mode, image, n_drawables, layer, args, data):
         grid.attach(spin, 1, 2, 1, 1)
         spin.show()
 
-        # Model Name parameter
         label = Gtk.Label.new_with_mnemonic(_("_Model Name"))
         grid.attach(label, 0, 0, 1, 1)
         label.show()
@@ -233,7 +199,6 @@ def run(procedure, run_mode, image, n_drawables, layer, args, data):
         grid.attach(combo, 1, 0, 1, 1)
         combo.show()
 
-        # Device Name parameter
         label = Gtk.Label.new_with_mnemonic(_("_Device Name"))
         grid.attach(label, 2, 0, 1, 1)
         label.show()
@@ -243,16 +208,12 @@ def run(procedure, run_mode, image, n_drawables, layer, args, data):
         grid.attach(combo, 3, 0, 1, 1)
         combo.show()
 
-        # Show Logo
         logo = Gtk.Image.new_from_file(image_paths["logo"])
-        # grid.attach(logo, 0, 0, 1, 1)
         vbox.pack_start(logo, False, False, 1)
         logo.show()
 
-        # Show License
         license_text = _("PLUGIN LICENSE : Apache-2.0")
         label = Gtk.Label(label=license_text)
-        # grid.attach(label, 1, 1, 1, 1)
         vbox.pack_start(label, False, False, 1)
         label.show()
 
@@ -260,7 +221,6 @@ def run(procedure, run_mode, image, n_drawables, layer, args, data):
         vbox.add(progress_bar)
         progress_bar.show()
 
-        # Wait for user to click
         dialog.show()
         while True:
             response = dialog.run()
@@ -269,12 +229,9 @@ def run(procedure, run_mode, image, n_drawables, layer, args, data):
                 device_name = config.get_property("device_name")
                 model_name = config.get_property("model_name")
 
-
                 result = superresolution(
                     procedure, image, layer, scale, device_name, model_name, progress_bar, config_path_output
                 )
-                # super_resolution(procedure, image, n_drawables, layer, force_cpu, progress_bar, config_path_output)
-                # If the execution was successful, save parameters so they will be restored next time we show dialog.
                 if result.index(0) == Gimp.PDBStatusType.SUCCESS and config is not None:
                     config.end_run(Gimp.PDBStatusType.SUCCESS)
                 return result
@@ -288,10 +245,7 @@ def run(procedure, run_mode, image, n_drawables, layer, args, data):
                     Gimp.PDBStatusType.CANCEL, GLib.Error()
                 )
 
-
 class Superresolution(Gimp.PlugIn):
-    ## Parameters ##
-    
     __gproperties__ = {
         "scale": (float, _("_Scale"), "Scale", 1, 4, 2, GObject.ParamFlags.READWRITE),
         "model_name": (
@@ -310,12 +264,10 @@ class Superresolution(Gimp.PlugIn):
         ),
     }
 
-    ## GimpPlugIn virtual methods ##
-
     def do_query_procedures(self):
         try:
             self.set_translation_domain(
-                 "gimp30-python", Gio.file_new_for_path(Gimp.locale_directory())
+                "gimp30-python", Gio.file_new_for_path(Gimp.locale_directory())
             )
         except:
             print("Error in set_translation_domain. This is expected if running GIMP 2.99.11 or later")
@@ -333,9 +285,7 @@ class Superresolution(Gimp.PlugIn):
             procedure.set_image_types("*")
             procedure.set_documentation(
                 N_("superresolution on the current layer."),
-                globals()[
-                    "__doc__"
-                ],  # This includes the docstring, on the top of the file
+                globals()["__doc__"],
                 name,
             )
             procedure.set_menu_label(N_("SuperResolution..."))
@@ -345,6 +295,5 @@ class Superresolution(Gimp.PlugIn):
             procedure.add_argument_from_property(self, "device_name")
             procedure.add_argument_from_property(self, "model_name")
         return procedure
-
 
 Gimp.main(Superresolution.__gtype__, sys.argv)
