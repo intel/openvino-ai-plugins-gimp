6a37574d4407fa0c229374bbe4b3ea0362da6a79
Long, Garth <garth.long@intel.com>
2025-10-01 08:06:38 -0700

    Update to reduce warnings in Linux

--- gimpopenvino/plugins/fastsd_ov/fastsd_ov.py
+++ gimpopenvino/plugins/fastsd_ov/fastsd_ov.py
@@ -6,6 +6,9 @@
 import gi
 
 gi.require_version("Gimp", "3.0")
+gi.require_version("GimpUi", "3.0")
+gi.require_version("Gtk", "3.0")
+
 from gi.repository import Gimp, Gio, GLib, Pango, Gtk
 
 import json
